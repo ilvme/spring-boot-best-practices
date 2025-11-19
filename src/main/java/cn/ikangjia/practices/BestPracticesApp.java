e83b37677f00e1f4a262d64703f3484453965c5c
kangjia <ikangjia.cn@outlook.com>
2025-11-19 15:17:55 +0800

    init...

---
+++ src/main/java/cn/ikangjia/practices/BestPracticesApp.java
@@ -0,0 +1,13 @@
+package cn.ikangjia.practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPracticesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPracticesApp.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ikangjia/practices/SpringBootBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ikangjia.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
